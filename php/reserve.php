b7557e7d85b27137d9b8264d4dd009e02f3634bb
Moontes <108738026+Moontes@users.noreply.github.com>
2022-12-11 10:29:05 +0100

    Update reserve.php

--- php/reserve.php
+++ php/reserve.php
@@ -1,16 +1,17 @@
 <?php
 
 session_start();
-
+//sprawdzenie czy metoda POST zwraca dane (nie jest pusta)
 if (!empty($_POST)) {
-    $customerID = $_SESSION["customerID"];
-    $item_id = $_POST['item'];
+    $customerID = $_SESSION["customerID"]; //przypisywanie id klienta ze zmiennej globalnej $_SESSION, czyli przypisuje aktualnie zalogowaną osobę
+    //pobranie danych z formularza i przypisanie do zmiennej
+    $item_id = $_POST['item']; 
     $termin = $_POST['date'];
     $days = $_POST['days'];
     $hours = $_POST['hours'];
 
     echo $_SESSION["customerID"];
-
+    //sprawdzenie czy wybrane daty przy skladaniu zamowienia nie sa starsze niz teraz i okres wypozyczenia jest wiekszy niz 14 dni
     $today = date('Y-m-d');
     $end_date = date('Y-m-d', strtotime($today . '+ 13 days'));
     if ($termin < $today || $termin > $end_date) {
@@ -23,6 +24,6 @@ if (!empty($_POST)) {
         die('niepoprawna liczba godzin!');
     }
 
-    require('functions.php');
+    require('functions.php'); 
     reservation($customerID, $item_id, $termin, $days, $hours);
 }
